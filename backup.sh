4a2fc4452ae5d953426ca609106d3babba04e619
root at v2 <root@v2>
2015-02-17 14:51:08 +0000

    run meteor update in pre, git-commit backups after mongodump

--- backup.sh
+++ backup.sh
@@ -2,3 +2,8 @@
 
 cd /data
 mongodump --port 81;
+git config --local user.email "backups@edit.tosdr.org"
+git config --local user.name "backup script inside edit.tosdr.org container"
+git config --local defaul.push current
+git add .
+git commit -am"backup @ edit.tosdr.org `date`"
