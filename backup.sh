a7dac57c79aa490237725ce5afa5f58c021ff86b
root user inside edit.tosdr.org <root@edit.tosdr.org>
2015-03-24 13:45:53 +0000

    git part is now done by snickers

--- backup.sh
+++ backup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 cd /data
-mongodump --port 81;
-git config --local user.email "backups@edit.tosdr.org"
-git config --local user.name "backup script inside edit.tosdr.org container"
-git config --local defaul.push current
-git add .
-git commit -am"backup @ edit.tosdr.org `date`"
+mongodump --port 81
