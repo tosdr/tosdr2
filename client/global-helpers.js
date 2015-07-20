51bdf326c0895c71a6f90c5dd3051bc9f8f500c6
pierreozoux <pierre@ozoux.net>
2015-07-20 17:01:59 +0100

    Better message for non verified users

--- client/global-helpers.js
+++ client/global-helpers.js
@@ -1,4 +1,5 @@
 Template.registerHelper('isModerator', utils.isModerator);
+Template.registerHelper('isVerified', utils.isVerified);
 
 Template.registerHelper('currentUserEmailAddress', function() {
   return Meteor.user().emails[0].address;
